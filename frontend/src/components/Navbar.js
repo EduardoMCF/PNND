62d4f3f5db77a8fc429fb5d2aaff8739429fd6f3
Eduardo C <eduardo.freitas@ccc.ufcg.edu.br>
2020-09-20 22:25:33 -0300

    refactoring components

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({menu}) => {
 
@@ -7,24 +8,31 @@ const Navbar = ({menu}) => {
 		const openbtn = document.getElementById("openbtn");
 		const closebtn = document.getElementById("closebtn");
 		const sidebar = document.getElementById("sidebar");
-		const Root = document.getElementById("root")
+		const Root = document.getElementById("root");
+		const routes = [...sidebar.getElementsByClassName('routes')];
 
-		closebtn.onclick = () => {
-			sidebar.style.width = "0";
-  			Root.style.marginLeft= "0";
-  			openbtn.innerHTML = "☰";
-  			openbtn.style.fontsize = "20px";
-  			openbtn.style.padding = "10px 15px";
+		console.log(document.getElementById('sidebar'));
 
-		}
-
-		openbtn.onclick = () => {
+		const open = () => {
 			sidebar.style.width = "250px";
   			Root.style.marginLeft= "250px";
   			openbtn.style.fontsize = "0px";
   			openbtn.style.padding = "0px";
   			openbtn.innerHTML = "";
 		}
+
+		const close = () => {
+			sidebar.style.width = "0";
+  			Root.style.marginLeft= "0";
+  			openbtn.innerHTML = "☰";
+  			openbtn.style.fontsize = "20px";
+  			openbtn.style.padding = "10px 15px";
+		}
+
+		closebtn.onclick = close;
+		openbtn.onclick = open;
+
+		routes.map(elem => elem.onclick = close);
 	},[])
 
 	return (
@@ -32,7 +40,7 @@ const Navbar = ({menu}) => {
 			<button id="openbtn">☰</button>
 			<nav id='sidebar' class="sidebar">
 			  <a href="javascript:void(0)" id="closebtn">×</a>
-			  {	menu.map(({section, href}) => <a href={href}> {section} </a>) }
+			  {	menu.map(({section, route}) => <Link to={route} className='routes'> {section} </Link>) }
 			</nav>
 		</div>
 	)
